237fd5c8aa0ebc4ff7cf2177d18b33777a73b0d6
Varun Urs M S <varunms.cs20@bmsce.ac.in>
2023-01-01 19:27:18 +0530

    Organised Components into respective folders Made Resources Page

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,10 +5,21 @@ import ContactsIcon from '@mui/icons-material/Contacts';
 import HeadsetMicIcon from '@mui/icons-material/HeadsetMic';
 import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react";
 
 const Navbar = () =>{
   let curDate = new Date().toLocaleDateString();
   let curTime = new Date().toLocaleTimeString();
+  
+  const [cTime,setCTime] = useState(curTime);
+  const [cDate,setCDate] = useState(curDate);
+  
+  const showTime = () => {
+    curTime = new Date().toLocaleTimeString();
+    setCTime(curTime);
+  }
+
+  setInterval(showTime,1000);
 
     return(
         <>
@@ -25,7 +36,7 @@ const Navbar = () =>{
         </div>
         <div className="date_time">
           <div className="date">{curDate}</div>
-          <div className="time">{curTime}</div>
+          <div className="time">{cTime}</div>
         </div>
         <img className="avatar" src={user} alt="avatar" />
       </div>
