cabb9bed7d287433528574f9648d3e4be2b2333b
o83-program <84218325+o83-program@users.noreply.github.com>
2023-01-08 14:32:09 +0530

    Add files via upload

---
+++ src/ReactComponents/Central_Section/About1.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+
+const About1 = () => {
+  return (
+    <>
+   <div className="container emp-profile">
+    <form method="">
+      <div className="row">
+        <div className="col-md-4">
+         {/* <img src={img1} alt="pic" /> */}
+        </div>  
+        <div className="profile-head">
+          <h5>CAMPVERSE</h5>
+          <h6>Website for fetching resources</h6>
+          <p className="profile-rating mt-3 mb-5">RANKINGS</p>
+
+          <ul className=" nav nav-tabs" role="tablist">
+            <li className ="nav-item">
+              <a className="nav-link active" href="#">Active</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+          
+          </ul>
+
+        </div>
+   </div>
+   </form>
+   </div>
+
+
+
+    <About1/>
+ 
+    
+    </> )
+}
+
+export default About1
